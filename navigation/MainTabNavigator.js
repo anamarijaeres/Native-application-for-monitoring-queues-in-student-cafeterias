17888b4e55788a472ea26baa18b2c883faa66e31
dominikkos <57326283+dominikkos@users.noreply.github.com>
2020-01-04 15:52:50 +0100

    Promijenjena ikonica na HomeScreen-u

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -25,11 +25,7 @@ HomeStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "md-information-circle"
-      }
+      name={Platform.OS === "ios" ? "ios-home" : "md-home"}
     />
   )
 };
